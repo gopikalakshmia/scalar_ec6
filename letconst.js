1dc9a392a3ce7b7355595070764757f8c8787255
gopikalakshmia <gopikalakshmia@gmail.com>
2024-06-11 23:16:39 -0700

    first

---
+++ letconst.js
@@ -0,0 +1,19 @@
+//reassignment
+let a=10;
+//let a=20  give error
+a=30;  //we can reassign
+
+
+console.log(a);
+if(a>20){
+    let b=20;
+    console.log(b);
+}
+//console.log(b); --let is blcok scope
+
+
+//const
+
+const bc=10;
+//bc=30;-- canot redclare and re assign
+console.log(bc);
